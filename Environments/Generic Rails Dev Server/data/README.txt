c7269656056c4ce56490ac64b9691c7a60230a85
Justin Turner <justinrayturner@gmail.com>
2013-05-30 09:49:38 -0600

    Rails DEV Environment
    An environment to develop ruby on rails applications in. Features RVM.

---
+++ Environments/Generic Rails Dev Server/data/README.txt
@@ -0,0 +1,5 @@
+This directory is available in the VM located at /opt/data.
+
+Do not commit anything in this directory to the DevInABox Project.
+
+Have A Nice Day and Happy Developing!
---
+++ Environments/Generic Rails Dev Server/puppet/manifests/development.pp
@@ -0,0 +1,32 @@
+stage { 'req-install': before => Stage['rvm-install'] }
+
+class requirements {
+  group { "puppet": ensure => "present", }
+  exec { "apt-update":
+    command => "/usr/bin/apt-get -y update"
+  }
+
+  package {
+    ["mysql-client", "mysql-server", "libmysqlclient-dev"]: 
+      ensure => installed, require => Exec['apt-update']
+  }
+}
+
+class installrvm {
+  include rvm
+  rvm::system_user { vagrant: ; }
+
+  if $rvm_installed == "true" {
+    rvm_system_ruby {
+      'ruby-1.9.3-p0':
+        ensure => 'present';
+    }
+  }
+}
+
+class doinstall {
+  class { requirements:, stage => "req-install" }
+  include installrvm
+}
+
+include doinstall
